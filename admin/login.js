22845dda29ce5ab846efaa979273f2251406b711
Matias <matiasdarosa742@gmail.com>
2026-03-25 00:57:40 -0300

    fix url

--- admin/login.js
+++ admin/login.js
@@ -9,7 +9,7 @@ const password = document.getElementById("password").value;
 
 try{
 
-const response = await fetch("https://sportcatalogapi-production.up.railway.app/api/apiAuth/login",{
+const response = await fetch("https://sportcatalogapi-production.up.railway.app/apiAuth/login",{
 method:"POST",
 headers:{
 "Content-Type":"application/json"
